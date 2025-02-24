509ae0173e6506b193edefa6151f2dcb5a7e793d
Lucas JOLY <lucas.joly.0903@gmail.com>
2025-02-24 10:26:14 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Fragment, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Hello from "./Hello";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Fragment>
+      <Hello />
+    </Fragment>
+  );
+}
+
+export default App;
